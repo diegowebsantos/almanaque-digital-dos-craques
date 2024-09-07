32abbf9e1d6e1268914c540d4b871612c43630a8
Diego Pereira da Silva <diegowebsantos@gmail.com>
2024-09-07 03:57:57 -0300

    novos dados e atributos adicionados

--- js/dados.js
+++ js/dados.js
@@ -3,42 +3,49 @@ let dados = [
         atleta: "Pelé",
         foto: "img/atletas/img-pele.jpg",
         descricao: "O Rei do Futebol Pelé, nascido Edson Arantes do Nascimento, é indiscutivelmente o maior jogador de futebol de todos os tempos. Sua carreira, marcada por uma habilidade técnica excepcional e uma paixão inigualável pelo esporte, o consagrou como um ícone global.",
-        link: "https://pt.wikipedia.org/wiki/Pel%C3%A9"
+        link: "https://pt.wikipedia.org/wiki/Pel%C3%A9",
+        tags: "rei rei pelé rei do futebol edson arantes do nascimento atacante atacantes"
     },
     {
         atleta: "Coutinho",
-        foto: "img/atletas/img-coutinho.jpg", // Substitua pelo caminho da imagem
+        foto: "img/atletas/img-coutinho.jpg",
         descricao: "Coutinho, um dos maiores meias da história do Santos, era conhecido por sua visão de jogo e passes precisos.",
-        link: "https://pt.wikipedia.org/wiki/Coutinho_(futebolista)#:~:text=Ant%C3%B4nio%20Wilson%20Vieira%20Hon%C3%B3rio%2C%20mais,brasileiro%20que%20atuou%20como%20atacante.&text=Data%20de%20nasc.&text=Local%20de%20nasc.&text=Ao%20lado%20de%20Pel%C3%A9%2C%20Pepe,artilheiro%20da%20hist%C3%B3ria%20do%20Santos." // Substitua pelo link da página sobre Coutinho
+        link: "https://pt.wikipedia.org/wiki/Coutinho_(futebolista)#:~:text=Ant%C3%B4nio%20Wilson%20Vieira%20Hon%C3%B3rio%2C%20mais,brasileiro%20que%20atuou%20como%20atacante.&text=Data%20de%20nasc.&text=Local%20de%20nasc.&text=Ao%20lado%20de%20Pel%C3%A9%2C%20Pepe,artilheiro%20da%20hist%C3%B3ria%20do%20Santos.",
+        tags: "Antônio Wilson Vieira Honório atacante atacantes" 
     },
     {
         atleta: "Pepe",
         foto: "img/atletas/img-pepe.jpg",
-        descricao: "Pepe, zagueiro imponente, foi fundamental para a conquista de diversos títulos pelo Santos.",
-        link: "https://pt.wikipedia.org/wiki/Gil_(futebolista)"
+        descricao: "Como jogador, atuou como ponta-esquerda e é considerado um dos maiores ídolos da história do Santos, única equipe em que defendeu de 1954 a 1969.",
+        link: "https://pt.wikipedia.org/wiki/Gil_(futebolista)",
+        tags: "José Macia canhão da vila ponta pontas ponta-esquerda"
     },
     {
         atleta: "Mengálvio",
         foto: "img/atletas/img-mengalvio.jpg",
         descricao: "Mengálvio, meia habilidoso, formou um trio de ataque memorável com Pelé e Coutinho.",
-        link: "https://pt.wikipedia.org/wiki/Meng%C3%A1lvio#:~:text=Foi%20bicampe%C3%A3o%20da%20Copa%20Intercontinental,Brasil%20conquistou%20o%20bicampeonato%20mundial."
+        link: "https://pt.wikipedia.org/wiki/Meng%C3%A1lvio#:~:text=Foi%20bicampe%C3%A3o%20da%20Copa%20Intercontinental,Brasil%20conquistou%20o%20bicampeonato%20mundial.",
+        tags: "Mengálvio Pedro Figueiró meia meias meio-campista"
     },
     {
         atleta: "Zito",
         foto: "img/atletas/img-zito.jpg",
         descricao: "Zito, volante marcador, era conhecido por sua força física e liderança em campo.",
-        link: "https://pt.wikipedia.org/wiki/Zito_(futebolista)#:~:text=Atuou%20no%20time%20por%20quinze,mais%20marcou%20gols%20pelo%20Alvinegro."
+        link: "https://pt.wikipedia.org/wiki/Zito_(futebolista)#:~:text=Atuou%20no%20time%20por%20quinze,mais%20marcou%20gols%20pelo%20Alvinegro.",
+        tags: "José Ely de Miranda volante volantes capitão capitães"
     },
     {
         atleta: "Clodoaldo",
         foto: "img/atletas/img-clodoaldo.jpg",
         descricao: "Clodoaldo, volante de grande qualidade técnica, foi fundamental para a conquista da Copa de 1970.",
-        link: "https://pt.wikipedia.org/wiki/Clodoaldo_(futebolista)#:~:text=Nos%20catorze%20anos%20em%20que,dos%20Campe%C3%B5es%20Intercontinentais%20de%201968."
+        link: "https://pt.wikipedia.org/wiki/Clodoaldo_(futebolista)#:~:text=Nos%20catorze%20anos%20em%20que,dos%20Campe%C3%B5es%20Intercontinentais%20de%201968.",
+        tags: "Clodoaldo Tavares de Santana volante volantes"
     },
     {
         atleta: "Edu",
         foto: "img/atletas/img-edu.jpg",
         descricao: "Edu era um jogador de extrema habilidade e chutes precisos. Muito temido por seus dribles, Edu figura entre os maiores dribladores de todos os tempos.",
-        link: "https://pt.wikipedia.org/wiki/Jonas_Eduardo_Am%C3%A9rico"
+        link: "https://pt.wikipedia.org/wiki/Jonas_Eduardo_Am%C3%A9rico",
+        tags: "Jonas Eduardo Américo ponta pontas ponta-esquerda"
     }
 ];
