a64c78e30a87d090665d81881b86e96e6f3b7184
Diego Pereira da Silva <diegowebsantos@gmail.com>
2024-09-07 03:57:17 -0300

    novas funções adicionadas

--- js/app.js
+++ js/app.js
@@ -1,14 +1,32 @@
 function pesquisar() {
   // Obtém a seção HTML onde os resultados da pesquisa serão exibidos
   let section = document.getElementById("resultados-pesquisa");
-  console.log(section); // Loga o elemento HTML no console para verificação
+
+  let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+  // se campoPesquisa for uma string sem nada
+  if (!campoPesquisa) {
+    section.innerHTML = "<p>O campo de pesquisa está vazio, <strong>digite o nome de um Atleta!</strong></p>"
+    return
+  }
+
+  campoPesquisa = campoPesquisa.toLowerCase()
 
   // Inicializa uma string vazia para armazenar os resultados da pesquisa
   let resultados = "";
+  let atleta = "";
+  let descricao = "";
+  let tags = "";
 
   // Itera sobre cada dado na lista de dados (assumindo que 'dados' é um array)
   for (let dado of dados) {
-    // Constrói o HTML para cada item do resultado da pesquisa, formatando os dados
+    atleta = dado.atleta.toLowerCase()
+    descricao = dado.descricao.toLowerCase()
+    tags = dado.tags.toLowerCase()
+    // se atleta includes campoPesquisa
+    if (atleta.includes(campoPesquisa) || tags.includes(campoPesquisa)){
+    //if (atleta.includes(campoPesquisa) || descricao.includes(campoPesquisa))
+    // cria um novo elemento
     resultados += `
     <div class="container item-resultado">
       <h2>
@@ -19,6 +37,12 @@ function pesquisar() {
       <a href="${dado.link}" target="_blank">Mais informações</a>
     </div>
     `;
+    }
+   
+  }
+
+  if (!resultados) {
+    resultados = "<p><strong>Nada</strong> foi encontrado</p>";
   }
 
   // Atribui o HTML gerado para a seção de resultados
